e4697ce2e2368dce72b81c0cb0c41c5ee45a939d
Shreya Ramtirth <shreyaramtirth4@gmail.com>
2022-07-17 20:12:30 +0530

    dashboard added

--- pages/donor/dashboard.js
+++ pages/donor/dashboard.js
@@ -1,12 +1,198 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
+import React from "react";
+import PropTypes from "prop-types";
+import Image from "next/image";
 function dashboard(props) {
   return (
-    <div>dashboard</div>
-  )
+    <div className="container-fluid">
+      <div className="row flex-nowrap">
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark position-fixed ">
+          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+            <a
+              href="/"
+              className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+            >
+              <span className="fs-5 d-none d-sm-inline">Menu</span>
+            </a>
+            <ul className="nav nav-pills flex-column mb-auto">
+              <li className="nav-item">
+                <a href="#" className="nav-link active" aria-current="page">
+                  <i className="fas fa-home"></i> &nbsp; Dashboard
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link text-white">
+                  <i className="fas fa-hand-holding-medical"></i> &nbsp; Your
+                  donates
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link text-white">
+                  <i className="fas fa-sign-out-alt"></i> &nbsp; Logout
+                </a>
+              </li>
+            </ul>
+            <hr />
+            <div className="dropdown pb-4">
+              <a
+                href="#"
+                className="d-flex align-items-center text-white text-decoration-none"
+                id="dropdownUser1"
+                aria-expanded="false"
+              >
+                <i className="fas fa-user"></i> &nbsp;
+                <span className="d-none d-sm-inline mx-1">loser</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col py-3 donor">
+          <form className="form row">
+            <div className="col-8">
+
+            <div className="mb-3">
+              <div className="row justify-content-center">
+                <div className="col-6">
+                  <h2>Food Donation Form</h2>
+                </div>
+                <div className="col-6">
+                  {/* <Image
+                    src="/images/foodbank.webp"
+                    height="200px"
+                    width="200px"
+                  /> */}
+                </div>
+              </div>
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Item
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Quantity
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                id="exampleInputPassword1"
+                />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Expiry (in hrs)
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                id="exampleInputPassword1"
+                />
+            </div>
+            <div className="mb-3">
+              <button className="btn btn-primary btn-block">
+                <i className="fas fa-plus"></i> &nbsp; ADD
+              </button>
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Address
+              </label>
+              <textarea
+                className="form-control"
+                placeholder="Enter Address here"
+                id="floatingTextarea2"
+                ></textarea>
+            </div>
+            <div className="row mt-5  mx-3">
+              <table className="table table-bordered">
+                <thead>
+                  <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Item Name</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Date Added</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Bootstrap 4 CDN and Starter Template</td>
+                    <td>Cristina</td>
+                    <td>2.846</td>
+                    <td>
+                      <button type="button" className="btn btn-danger">
+                        <i className="far fa-trash-alt"></i>
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Bootstrap Grid 4 Tutorial and Examples</td>
+                    <td>Cristina</td>
+                    <td>3.417</td>
+                    <td>
+                      <button type="button" className="btn btn-danger">
+                        <i className="far fa-trash-alt"></i>
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Bootstrap Flexbox Tutorial and Examples</td>
+                    <td>Cristina</td>
+                    <td>1.234</td>
+                    <td>
+                      <button type="button" className="btn btn-danger">
+                        <i className="far fa-trash-alt"></i>
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="input-group mt-3 mx-3">
+              <input
+                type="file"
+                className="form-control"
+                id="inputGroupFile04"
+                aria-describedby="inputGroupFileAddon04"
+                aria-label="Upload"
+                />
+              <button
+                className="btn btn-outline-primary"
+                type="button"
+                id="inputGroupFileAddon04"
+              >
+                Button
+              </button>
+            </div>
+            <button type="submit" className="btn btn-primary mt-3">
+              Submit
+            </button>
+                </div>
+                <div className="col-4">
+                    {/* <Image
+                    src="/images/slider-img1.png"
+                    height="1000px"
+                    width="1000px"
+                  /> */}
+                </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-dashboard.propTypes = {}
+dashboard.propTypes = {};
 
-export default dashboard
+export default dashboard;
--- pages/recipient/dashboard.js
+++ pages/recipient/dashboard.js
@@ -1,12 +1,222 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
+import React from "react";
+import PropTypes from "prop-types";
+import Image from "next/image";
 function dashboard(props) {
   return (
-    <div>dashboard</div>
-  )
+<div className="container-fluid">
+    <div className="row flex-nowrap">
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark position-fixed ">
+            <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                    <span className="fs-5 d-none d-sm-inline">Menu</span>
+                </a>
+                <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="#" class="nav-link active" aria-current="page">
+        <i class="fas fa-home"></i> &nbsp;
+          Dashboard
+        </a>
+      </li>
+      <li class="nav-item" >
+        <a href="#" class="nav-link text-white">
+        <i class="fas fa-utensils"></i> &nbsp;
+          Your Orders
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link text-white">
+        <i class="fas fa-comment-dots"></i> &nbsp;
+          Feedback
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link text-white">
+        <i class="fas fa-sign-out-alt"></i> &nbsp;
+          Logout
+        </a>
+      </li>
+      
+    </ul>
+                <hr />
+                <div className="dropdown pb-4">
+                    <a href="#" className="d-flex align-items-center text-white text-decoration-none" id="dropdownUser1" aria-expanded="false">
+                    <i class="fas fa-user"></i> &nbsp;
+                        <span className="d-none d-sm-inline mx-1">loser</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div className="col py-3 food">
+            
+        <section class="product_section layout_padding2-top layout_padding-bottom">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          Available Foods
+        </h2>
+        <p>
+          which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't an
+        </p>
+      </div>
+      <div class="row">
+        <div class="col-sm-6 col-lg-4">
+          <div class="box">
+            <div class="img-box">
+              <Image src="/images/p1.png" alt=""  width="230px" height="200px" />
+            </div>
+            <div class="detail-box">
+              <span class="rating">
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star-half-o" aria-hidden="true"></i>
+              </span>
+              <a href="">
+                Orange
+              </a>
+              <div class="price_box">
+                <h6 class="price_heading">
+                  <span>$</span> 10.00
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <div class="box">
+            <div class="img-box">
+              <Image src="/images/p2.png" alt=""  width="230px" height="200px"/>
+            </div>
+            <div class="detail-box">
+              <span class="rating">
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star-half-o" aria-hidden="true"></i>
+              </span>
+              <a href="">
+                Durian
+              </a>
+              <div class="price_box">
+                <h6 class="price_heading">
+                  <span>$</span> 10.00
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <div class="box">
+            <div class="img-box">
+              <Image src="/images/p3.png" alt=""  width="230px" height="200px"/>
+            </div>
+            <div class="detail-box">
+              <span class="rating">
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star-half-o" aria-hidden="true"></i>
+              </span>
+              <a href="">
+                Kiwi
+              </a>
+              <div class="price_box">
+                <h6 class="price_heading">
+                  <span>$</span> 10.00
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <div class="box">
+            <div class="img-box">
+              <Image src="/images/p4.png" alt=""  width="230px" height="200px"/>
+            </div>
+            <div class="detail-box">
+              <span class="rating">
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star-half-o" aria-hidden="true"></i>
+              </span>
+              <a href="">
+                Mango
+              </a>
+              <div class="price_box">
+                <h6 class="price_heading">
+                  <span>$</span> 10.00
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <div class="box">
+            <div class="img-box">
+              <Image src="/images/p5.png" alt=""  width="230px" height="200px"/>
+            </div>
+            <div class="detail-box">
+              <span class="rating">
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star-half-o" aria-hidden="true"></i>
+              </span>
+              <a href="">
+                Banana
+              </a>
+              <div class="price_box">
+                <h6 class="price_heading">
+                  <span>$</span> 10.00
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <div class="box">
+            <div class="img-box">
+              <Image src="/images/p6.png" alt=""  width="230px" height="200px"/>
+            </div>
+            <div class="detail-box">
+              <span class="rating">
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star-half-o" aria-hidden="true"></i>
+              </span>
+              <a href="">
+                Apple
+              </a>
+              <div class="price_box">
+                <h6 class="price_heading">
+                  <span>$</span> 10.00
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="btn-box">
+        <a href="">
+          View All
+        </a>
+      </div> /
+    </div>
+  </section>
+        </div>
+    </div>
+</div>
+  );
 }
 
-dashboard.propTypes = {}
+dashboard.propTypes = {};
 
-export default dashboard
+export default dashboard;
